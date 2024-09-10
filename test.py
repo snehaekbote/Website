d60adc67afe5f8e9739975ceb1e86aa2eac5e7a9
snehaekbote <112148666+snehaekbote@users.noreply.github.com>
2024-09-10 12:05:40 +0530

    Update test file

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
+import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.exc import OperationalError
 
-# Replace with your actual connection string
 DATABASE_URL = "mysql+pymysql://admin:Admin#235@13.234.113.49/turtu_website"
 
-try:
-    engine = create_engine(DATABASE_URL)
-    connection = engine.connect()
-    print("Connection to MySQL database was successful!")
-    connection.close()
-except OperationalError as e:
-    print(f"SQLAlchemy connection error: {e}")
-except NameError as e:
-    print(f"Error: {e}")
+def test_database_connection():
+    try:
+        engine = create_engine(DATABASE_URL)
+        with engine.connect() as connection:
+            assert connection is not None
+    except OperationalError as e:
+        pytest.fail(f"SQLAlchemy connection error: {e}")
+    except Exception as e:
+        pytest.fail(f"An error occurred: {e}")
