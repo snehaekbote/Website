8e073cf10db4a9f6b5777a83406e99c498bb02cb
snehaekbote <snehaekbote13@gmail.com>
2024-09-17 15:22:31 +0530

    Fix TypeError by adding default value for SMTP_PORT

--- config/config.py
+++ config/config.py
@@ -10,7 +10,7 @@ class Config:
     DB_PASSWORD = os.getenv('DB_PASSWORD')
     DB_NAME = os.getenv('DB_NAME')
     SMTP_SERVER = os.getenv('SMTP_SERVER')
-    SMTP_PORT = int(os.getenv('SMTP_PORT', 587))
+    SMTP_PORT = int(os.getenv('SMTP_PORT', 25))
     SMTP_USERNAME = os.getenv('SMTP_USERNAME')
     SMTP_PASSWORD = os.getenv('SMTP_PASSWORD')
     SECRET_KEY = os.getenv('SECRET_KEY')
